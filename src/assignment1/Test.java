ffac20404ab5dcdab71e294d7732bc1e1f8cc173
Thomas Rieder <tho.rieder@aon.at>
2012-10-10 16:19:13 +0200

    added skelton for output

--- src/assignment1/Test.java
+++ src/assignment1/Test.java
@@ -6,7 +6,14 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("TODO");
+		System.out.println("Band Test");
+		// generate band objects
+		
+		System.out.println("Event Test");
+		// generate event objects
+		
+		System.out.println("Complete Test");
+		// generate everything
 	}
 
 }
