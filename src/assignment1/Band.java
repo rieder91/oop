014a8c79640c3d158be432ab999834cb73f424a1
Thomas Rieder <tho.rieder@aon.at>
2012-10-11 20:01:43 +0200

    added history

--- src/assignment1/Band.java
+++ src/assignment1/Band.java
@@ -8,16 +8,19 @@ public class Band {
 
 	private String name;
 	private String genre;
-
+	
+	// contain the current information
 	private ArrayList<Event> events;
 	private ArrayList<Member> members;
 	private ArrayList<Track> tracks;
-
-	private HashMap<Member, Date> memberDates;
-	private HashMap<Track, Date> trackDates;
-
-	private HashMap<Member, Date> previousMembers;
-	private HashMap<Track, Date> previousTracks;
+	
+	// contain the "join dates"
+	private HashMap<Member, ArrayList<Date>> memberDates;
+	private HashMap<Track, ArrayList<Date>> trackDates;
+	
+	// contain the "leave dates"
+	private HashMap<Member, ArrayList<Date>> previousMembers;
+	private HashMap<Track, ArrayList<Date>> previousTracks;
 
 
 
@@ -25,6 +28,16 @@ public class Band {
 		super();
 		this.name = name;
 		this.genre = genre;
+		
+		events = new ArrayList<Event>();
+		members = new ArrayList<Member>();
+		tracks = new ArrayList<Track>();
+		
+		memberDates = new HashMap<Member, ArrayList<Date>>();
+		trackDates = new HashMap<Track, ArrayList<Date>>();
+		
+		previousMembers = new HashMap<Member, ArrayList<Date>>();
+		previousTracks = new HashMap<Track, ArrayList<Date>>();
 	}
 
 	public ArrayList<Member> getMembers() {
@@ -51,16 +64,19 @@ public class Band {
 		ret += "\n\nMembers:\n";
 		for(Member m : members) {
 			ret += m.toString();
+			ret += '\n';
 		}
 		
-		ret+= "\n\nEvents:\n";
+		ret+= "\nEvents:\n";
 		for(Event e : events) {
 			ret += e.toString();
+			ret += '\n';
 		}
 		
-		ret+= "\n\nTracks:\n";
+		ret+= "\nTracks:\n";
 		for(Track t : tracks) {
 			ret += t.toString();
+			ret += '\n';
 		}
 
 		return ret;
@@ -70,19 +86,51 @@ public class Band {
 	/**
 	 * 
 	 * @param t
+	 * @param d
 	 */
 	public void addTrack(Track t, Date d) {
-		tracks.add(t);
-		trackDates.put(t, d);
+		if(!tracks.contains(t)) {
+			tracks.add(t);
+			if(trackDates.containsKey(t)) {
+				// the track has already been added in the past - we need to add a new date
+				// TODO Check if the LAST leave-date was BEFORE the new join date
+				trackDates.get(t).add(d);
+			} else {
+				ArrayList<Date> newHistory = new ArrayList<Date>();
+				newHistory.add(d);
+				trackDates.put(t, newHistory);
+			}
+		} else {
+			// throw new already exists exception
+		}
+		
 	}
 
 	/**
 	 * 
 	 * @param t
+	 * @param d
 	 */
 	public void removeTrack(Track t, Date d) {
-		tracks.remove(t);
-		previousTracks.put(t, d);
+		ArrayList<Date> history = trackDates.get(t);
+		Date joinDate = history.get(history.size());
+		
+		if(joinDate.after(d)) {
+			// throw new remove date before add date exception
+		} else if(!tracks.contains(t)) {
+			// throw new invalid track exception
+		} else {
+			tracks.remove(t);
+			if(previousTracks.containsKey(t)) {
+				// we need to add a new date to the history
+				previousTracks.get(t).add(d);
+			} else {
+				ArrayList<Date> newHistory = new ArrayList<Date>();
+				newHistory.add(d);
+				previousTracks.put(t, newHistory);
+			}
+			
+		}
 	}
 
 	/**
@@ -91,7 +139,12 @@ public class Band {
 	 * @return
 	 */
 	public void addEvent(Event e) {
-		events.add(e);
+		if(!events.contains(e)) {
+			events.add(e);
+		} else {
+			// throw new already exists exception
+		}
+		
 	}
 
 	/**
@@ -100,8 +153,12 @@ public class Band {
 	 * @return
 	 */
 	public void removeEvent(Event e) {
-		events.remove(e);
-	}
+		if(!events.contains(e)) {
+			// throw new invalid event exception
+		} else {
+			events.remove(e);
+		}
+	}	
 
 	/**
 	 * 
@@ -109,8 +166,20 @@ public class Band {
 	 * @return
 	 */
 	public void addMember(Member m, Date d) {
-		members.add(m);
-		memberDates.put(m, d);
+		if(!members.contains(m)) {
+			members.add(m);
+			if(memberDates.containsKey(m)) {
+				// the member has already been part of the band once before
+				// TODO Check if the LAST leave-date was BEFORE the new join date
+				memberDates.get(m).add(d);
+			} else {
+				ArrayList<Date> newHistory = new ArrayList<Date>();
+				newHistory.add(d);
+				memberDates.put(m, newHistory);
+			}
+		} else {
+			// throw new already exists exception
+		}
 	}
 
 	/**
@@ -119,8 +188,23 @@ public class Band {
 	 * @return
 	 */
 	public void removeMember(Member m, Date d) {
-		members.remove(m);
-		previousMembers.put(m, d);
+		ArrayList<Date> history = memberDates.get(m);
+		Date joinDate = history.get(history.size());
+		if(joinDate.after(d)) {
+			// throw new remove date before add date exception
+		} else if(!members.contains(m)) {
+			// throw new invalid member exception
+		} else {
+			members.remove(m);
+			if(previousMembers.containsKey(m)) {
+				// the member has leave once before
+				previousMembers.get(m).add(d);
+			} else {
+				ArrayList<Date> newHistory = new ArrayList<Date>();
+				newHistory.add(d);
+				previousMembers.put(m, newHistory);
+			}
+		}
 	}
 
 	/**
@@ -168,20 +252,28 @@ public class Band {
 	 */
 	public ArrayList<Member> getMemberAt(Date d) {
 		ArrayList<Member> ret = new ArrayList<Member>();
-
+		
 		for(Member m : memberDates.keySet()) {
-			// check if the member joined before the date
-			if(memberDates.get(m).before(d)) {
-				// check if the member left the band
-				if(previousMembers.containsKey(m)) {
-					// did the member leave before or after the given date?
-					if(previousMembers.get(m).after(d)) {
-						ret.add(m);
-					}
-				} else {
-					ret.add(m);
+			Date lastValidDate = null;
+			for(Date joinDate : memberDates.get(m)) {
+				if(joinDate.before(d)) {
+					lastValidDate = joinDate;
 				}
 			}
+			
+			// if he left the group get the one after the lastValidDate
+			if(lastValidDate != null && previousMembers.containsKey(m)) {
+				for(Date leaveDate : previousMembers.get(m)) {
+					if(leaveDate.before(d) && leaveDate.after(lastValidDate)) {
+						lastValidDate = null;
+					}
+				}
+			}
+			
+			if(lastValidDate != null) {
+				ret.add(m);
+			}
+			
 		}
 		return ret;
 	}
@@ -193,16 +285,25 @@ public class Band {
 	 */
 	public ArrayList<Track> getTracks(Date d) {
 		ArrayList<Track> ret = new ArrayList<Track>();
-
+		
 		for(Track t : trackDates.keySet()) {
-			if(trackDates.get(t).before(d)) {
-				if(previousMembers.containsKey(t)) {
-					if(previousMembers.get(t).after(d)) {
-						ret.add(t);
-					}
-				} else {
-					ret.add(t);
+			Date lastValidDate = null;
+			for(Date addDate : trackDates.get(t)) {
+				if(addDate.before(d)) {
+					lastValidDate = addDate;
 				}
+			}
+			
+			if(lastValidDate != null && previousTracks.containsKey(t)) {
+				for(Date removeDate : previousTracks.get(t)) {
+					if(removeDate.before(d) && removeDate.after(lastValidDate)) {
+						lastValidDate = null;
+					}
+				}
+			}
+			
+			if(lastValidDate != null) {
+				ret.add(t);
 			}
 		}
 
