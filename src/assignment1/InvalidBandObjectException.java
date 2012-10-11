f785025f9360d2d8aec706835d4f94bb833b87a2
Thomas Rieder <tho.rieder@aon.at>
2012-10-11 20:37:02 +0200

    rename

---
+++ src/assignment1/InvalidBandObjectException.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+@SuppressWarnings("serial")
+public class InvalidBandObjectException extends Exception {
+
+	public InvalidBandObjectException() {
+	}
+
+	public InvalidBandObjectException(String msg) {
+		super(msg);
+	}
+}
