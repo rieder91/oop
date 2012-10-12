967dc19b5b11bb907c9edb3ea38b9133e966ef9d
Thomas Rieder <tho.rieder@aon.at>
2012-10-12 13:08:29 +0200

    added time to Events

--- src/Test.java
+++ src/Test.java
@@ -13,7 +13,8 @@ public class Test {
 	 *            command line arguments
 	 */
 	public static void main(String[] args) {
-		SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+		SimpleDateFormat formatTime = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy");
 
 		try {
 			System.out.println("=== Band Test ===\n");
@@ -30,16 +31,18 @@ public class Test {
 			Member dominic = new Member("Dominic", "Riedl", "Drums",
 					"0699123456");
 
-			Gig novarock2010 = new Gig(format.parse("11.07.2010"),
+			Gig novarock2010 = new Gig(formatTime.parse("11.07.2010 12:00"),
 					"Pannonia Fields II", 72, 2500.0);
-			Gig novarock2011 = new Gig(format.parse("11.07.2011"),
+			Gig novarock2011 = new Gig(formatTime.parse("11.07.2011 13:00"),
 					"Pannonia Fields II", 72, 5000.0);
-			Gig novarock2012 = new Gig(format.parse("08.07.2012"),
+			Gig novarock2012 = new Gig(formatTime.parse("08.07.2012 14:00"),
 					"Pannonia Fields II", 72, 10000.0);
 
-			Rehearsal postNova2010 = new Rehearsal(format.parse("15.08.2010"),
+			Rehearsal postNova2010 = new Rehearsal(
+					formatTime.parse("15.08.2010 20:00"),
 					"Vienna Sound Studio", 5, 100.0);
-			Rehearsal postNova2011 = new Rehearsal(format.parse("15.08.2011"),
+			Rehearsal postNova2011 = new Rehearsal(
+					formatTime.parse("15.08.2011 20:00"),
 					"Vienna Sound Studio Mk II", 7, 1000.0);
 
 			Track thunderstruck = new Track("Thunderstruck", 321);
@@ -48,12 +51,12 @@ public class Test {
 			Track bloodBrothers = new Track("Blood Brothers", 214);
 			Track byob = new Track("B.Y.O.B.", 256);
 			Track myCurse = new Track("My Curse", 245);
-			
+
 			// Creating the band history
 			try {
-				ultraCoders.addMember(thomas, format.parse("17.06.2010"));
-				ultraCoders.addMember(markus, format.parse("02.01.2010"));
-				ultraCoders.addMember(dominic, format.parse("06.11.2009"));
+				ultraCoders.addMember(thomas, formatDate.parse("17.06.2010"));
+				ultraCoders.addMember(markus, formatDate.parse("02.01.2010"));
+				ultraCoders.addMember(dominic, formatDate.parse("06.11.2009"));
 
 				ultraCoders.addEvent(novarock2010);
 				ultraCoders.addEvent(novarock2011);
@@ -62,22 +65,27 @@ public class Test {
 				ultraCoders.addEvent(postNova2010);
 				ultraCoders.addEvent(postNova2011);
 
-				ultraCoders.addTrack(thunderstruck, format.parse("30.3.2010"));
-				ultraCoders.addTrack(stairway, format.parse("16.06.2011"));
-				ultraCoders.addTrack(prayer, format.parse("10.09.2011"));
-				ultraCoders.addTrack(bloodBrothers, format.parse("15.09.2011"));
-				ultraCoders.addTrack(byob, format.parse("30.11.2011"));
-				ultraCoders.addTrack(myCurse, format.parse("05.03.2012"));
+				ultraCoders.addTrack(thunderstruck,
+						formatDate.parse("30.3.2010"));
+				ultraCoders.addTrack(stairway, formatDate.parse("16.06.2011"));
+				ultraCoders.addTrack(prayer, formatDate.parse("10.09.2011"));
+				ultraCoders.addTrack(bloodBrothers,
+						formatDate.parse("15.09.2011"));
+				ultraCoders.addTrack(byob, formatDate.parse("30.11.2011"));
+				ultraCoders.addTrack(myCurse, formatDate.parse("05.03.2012"));
 
-				ultraCoders.removeMember(thomas, format.parse("25.05.2011"));
-				ultraCoders.addMember(thomas, format.parse("30.05.2011"));
-				ultraCoders.removeMember(thomas, format.parse("25.06.2011"));
-				ultraCoders.addMember(thomas, format.parse("30.08.2012"));
-				
-				ultraCoders.removeTrack(stairway, format.parse("20.04.2012"));
-				ultraCoders.removeTrack(byob, format.parse("10.03.2012"));
-				
-				ultraCoders.addTrack(stairway, format.parse("30.05.2012"));
+				ultraCoders
+				.removeMember(thomas, formatDate.parse("25.05.2011"));
+				ultraCoders.addMember(thomas, formatDate.parse("30.05.2011"));
+				ultraCoders
+				.removeMember(thomas, formatDate.parse("25.06.2011"));
+				ultraCoders.addMember(thomas, formatDate.parse("30.08.2012"));
+
+				ultraCoders.removeTrack(stairway,
+						formatDate.parse("20.04.2012"));
+				ultraCoders.removeTrack(byob, formatDate.parse("10.03.2012"));
+
+				ultraCoders.addTrack(stairway, formatDate.parse("30.05.2012"));
 			} catch (InvalidDateException e) {
 				System.out.println(e.getMessage());
 			} catch (InvalidBandObjectException e) {
@@ -88,27 +96,27 @@ public class Test {
 
 			// Several queries
 			try {
-				System.out.println(ultraCoders.getTracks(format
+				System.out.println(ultraCoders.getTracks(formatDate
 						.parse("29.05.2011")));
-				System.out.println(ultraCoders.getTracks(format
+				System.out.println(ultraCoders.getTracks(formatDate
 						.parse("29.04.2012")));
-				System.out.println(ultraCoders.getTracks(format
+				System.out.println(ultraCoders.getTracks(formatDate
 						.parse("30.06.2012")));
-				
-				System.out.println(ultraCoders.getMembers(format
+
+				System.out.println(ultraCoders.getMembers(formatDate
 						.parse("27.05.2011")));
-				System.out.println(ultraCoders.getMembers(format
+				System.out.println(ultraCoders.getMembers(formatDate
 						.parse("11.06.2011")));
-				System.out.println(ultraCoders.getMembers(format
+				System.out.println(ultraCoders.getMembers(formatDate
 						.parse("11.09.2012")));
-				
+
 				System.out.println(ultraCoders.getEvents(
-						format.parse("01.01.2010"), format.parse("01.01.2012"),
-						types));
-				
+						formatDate.parse("01.01.2010"),
+						formatDate.parse("01.01.2012"), types));
+
 				System.out.println(ultraCoders.getBilling(
-						format.parse("01.01.2010"), format.parse("01.01.2012"),
-						types));
+						formatDate.parse("01.01.2010"),
+						formatDate.parse("01.01.2012"), types));
 			} catch (InvalidDateException e) {
 				System.out.println(e.getMessage());
 			}
