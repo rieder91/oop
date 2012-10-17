74efad3ecc5aa654fc2e333c9c4bfd8ebd6a8b05
Thomas Rieder <tho.rieder@aon.at>
2012-10-18 00:49:11 +0200

    java-fucking-doc

--- src/auth/Authenticator.java
+++ src/auth/Authenticator.java
@@ -15,10 +15,10 @@ public class Authenticator {
 
 	/**
 	 * 
-	 * @param askingAuth
-	 * @param grantingAuth
-	 * @param m
-	 * @throws InsufficientPermissionsException
+	 * @param askingAuth the auth wich asks for permission
+	 * @param grantingAuth the auth from which permission is being asked
+	 * @param m the method which askingAuth wishes to invoke
+	 * @throws InsufficientPermissionsException if not allowed to invoke
 	 */
 	public static void checkPermissions(Authenticatable askingAuth,
 			Authenticatable grantingAuth, Method m)
@@ -29,7 +29,11 @@ public class Authenticator {
 			throw new InsufficientPermissionsException(
 					"Not allowed to invoke method");
 	}
-
+	
+	/**
+	 * debug method for printing the permission of a given auth-obj
+	 * @param auth auth-obj of which the permissions are printed
+	 */
 	public static void printPermissions(Authenticatable auth) {
 		HashMap<Authenticatable, Permission> roles = auth.getRoles();
 		System.out.println("\nRoles:");
