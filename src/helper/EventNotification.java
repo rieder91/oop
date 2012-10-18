64d9f5e208e8de5efdabe7f74df1a45003a30875
budddy <markus.zisser@gmx.at>
2012-10-18 21:18:48 +0200

    minor changes

--- src/helper/EventNotification.java
+++ src/helper/EventNotification.java
@@ -8,12 +8,33 @@ import band.Event;
  */
 public class EventNotification {
 
+	private final Event e;
+	private final Status status;
+
 	/**
 	 * @param e
 	 * @param status
 	 */
 	public EventNotification(final Event e, final Status status) {
 
+		this.e = e;
+		this.status = status;
+	}
+
+	/**
+	 * @return the event
+	 */
+	public Event getE() {
+
+		return this.e;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public Status getStatus() {
+
+		return this.status;
 	}
 
 }
