3d6eed079e3938c8916294b9743efdba30acc199
Thomas Rieder <tho.rieder@aon.at>
2012-10-18 23:45:00 +0200

    added hashcode and equals

--- src/band/Place.java
+++ src/band/Place.java
@@ -79,6 +79,38 @@ public class Place {
 			infrastructure.remove(i);
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((infrastructure == null) ? 0 : infrastructure.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Place other = (Place) obj;
+		if (infrastructure == null) {
+			if (other.infrastructure != null)
+				return false;
+		} else if (!infrastructure.equals(other.infrastructure))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	
 
 }
