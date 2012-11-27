0d69724f629a394e83f36270034ee3fc94394ce9
Thomas Rieder <tho.rieder@aon.at>
2012-11-27 23:33:34 +0100

    preparation for assignment 7

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-
-
 /**
- * Test class for Assignment 6
+ * Test class for Assignment 7
  * 
  * @author OOP Gruppe 187
  */
@@ -10,7 +7,7 @@ import java.util.ArrayList;
 public class Test {
 
 	/**
-	 * Test class for Assignment 6
+	 * Test class for Assignment 7
 	 * 
 	 * @param args
 	 *            command line arguments
@@ -18,32 +15,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out
 				.println("Objektorientierte Programmiertechniken                  \n"
-						+ "Datum: 21.11.2012                                      \n"
-						+ "Aufgabe 6                                              \n"
+						+ "Datum: 28.11.2012                                      \n"
+						+ "Aufgabe 7                                              \n"
 						+ "Gruppe 187: Dominic Riedl, Thomas Rieder, Markus Zisser\n");
-
-		/*
-		 * 
-		 * ANMERKUNG: alle Methoden die fuer die Simulation der Multimethoden
-		 * verwendet werden haben wir protected gemacht. Ohne Packages entfaltet
-		 * dies bei java nicht die gewuenschte Wirkung. Allerdings sollte so
-		 * erkennbar sein welche Methoden nicht direkt aufgerufen werden sollten.
-		 * 
-		 */
-		
-		ArrayList<Tester> tests = new ArrayList<Tester>();
-		tests.add(new SkinTester());
-		tests.add(new SoftwareTester());
-		tests.add(new SensorTester());
-		tests.add(new FactoryTester());
-		tests.add(new ReplacementTester());
-		
-		
-		for(Tester t : tests) {
-			System.out.println("=== Testing module: " + t.getName() + " ===");
-			t.runTests();
-			System.out.println();
-		}
-		
 	}
 }
