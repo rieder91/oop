c29e9fd17fae1e8ba57b51177e5d7afb1dd0e7ee
Thomas Rieder <tho.rieder@aon.at>
2012-12-08 23:35:34 +0100

    added bauernhof tester

--- src/Test.java
+++ src/Test.java
@@ -21,6 +21,7 @@ public class Test {
 		
 		
 		Liste myTests = new Liste();
+		myTests.add(new BauernhofTester());
 		myTests.add(new AnnotationTester());
 		
 		MyIterator it = myTests.iterator();
@@ -28,60 +29,5 @@ public class Test {
 		while(it.hasNext()) {
 			((Tester) it.next()).runTests();
 		}
-		
-//		Bauernhof b = new Bauernhof("Tony's Freilandeier");
-//		
-//		
-//		Traktor bio = new BiogasTraktor("Bio #1", 15);
-//		bio.setEinsatzart(new Duengerstreuer(5.5));
-//		
-//		Traktor t = new DieselTraktor("Diesel #1", 14);
-//		
-//		
-//		Traktor t2 = new DieselTraktor("Diesel #2", 1);
-//		t2.setEinsatzart(new Duengerstreuer(6.5));
-//		
-//		
-//		
-//		Traktor t3 = new DieselTraktor("Diesel #3", 3);
-//		t3.setEinsatzart(new Drillmaschine(10));
-//		
-//		Traktor t4 = new DieselTraktor("Diesel #4", 4);
-//		t4.setEinsatzart(new Drillmaschine(1));
-//		
-//		Traktor t5 = new BiogasTraktor("Bio #5", 5);
-//		t5.setEinsatzart(new Drillmaschine(2));
-//		
-//		Traktor t6 = new BiogasTraktor("Bio #6", 6);
-//		t6.setEinsatzart(new Drillmaschine(15));
-//		
-//		
-//		b.addTraktor(t);
-//		b.addTraktor(t2);
-//		b.addTraktor(t3);
-//		b.addTraktor(t4);
-//		b.addTraktor(t5);
-//		b.addTraktor(t6);
-//		b.addTraktor(bio);
-//		
-//		System.out.println(b);
-//		
-//		b.increaseHoursOfTraktor(15, 10);
-//		b.increaseDieselUsage(14, 10);
-//		b.increaseDieselUsage(15, 10);
-//		b.increaseDieselUsage(10, 10);
-//		b.increaseGasUsage(15, 10);
-//		b.increaseGasUsage(14, 10);
-//		b.increaseGasUsage(10, 10);
-//		
-//		b.changeUsageOfTraktor(14, new Drillmaschine(10));
-//		
-//		System.out.println(b);
-//		
-//		
-//		System.out.println(b.getSaescharenStats());
-//		
-//		System.out.println(b.getCapacityStats());
-		
 	}
 }
