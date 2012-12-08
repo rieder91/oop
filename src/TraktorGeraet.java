edb02ccf16dfc09007a1a7802e38ab4ec1957ecb
Thomas Rieder <tho.rieder@aon.at>
2012-12-08 23:35:43 +0100

    added comments

--- src/TraktorGeraet.java
+++ src/TraktorGeraet.java
@@ -1,5 +1,13 @@
-@Creator()
+/**
+ * Interface that all devices that can be attached to a Traktor implement
+ * @author Thomas
+ *
+ */
+@Creator(name = "Thomas", lastUpdate = "08.12.2012")
 public interface TraktorGeraet {
-	@Creator()
+	/**
+	 * @return returns the specific detail that each device has (i.e. Fassungsvolumen)
+	 */
+	@Creator(name = "Thomas", lastUpdate = "08.12.2012")
 	public Object getDetail();
 }
