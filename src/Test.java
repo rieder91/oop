ff2871a48b6e9cf8484e8a0b8165ad3d9ee2eea4
Thomas Rieder <tho.rieder@aon.at>
2012-12-12 15:46:23 +0100

    preparation for assignment 9

--- src/Test.java
+++ src/Test.java
@@ -1,36 +1,22 @@
 /**
- * Test class for Assignment 8
+ * Test class for Assignment 9
  * 
  * @author OOP Gruppe 187
  */
 
-@Creator(name = "Thomas", lastUpdate = "09.12.2012")
 public class Test {
 
 	/**
-	 * Test class for Assignment 8
+	 * Test class for Assignment 9
 	 * 
 	 * @param args
 	 *            command line arguments
 	 */
-	@Creator(name = "Thomas", lastUpdate = "09.12.2012")
 	public static void main(String[] args) {
 
 		System.out.println("Objektorientierte Programmiertechniken                  \n"
-				+ "Datum: 05.12.2012                                      \n"
-				+ "Aufgabe 8                                              \n"
+				+ "Datum: 12.12.2012                                      \n"
+				+ "Aufgabe 9                                              \n"
 				+ "Gruppe 187: Dominic Riedl, Thomas Rieder, Markus Zisser\n");
-		
-		
-		Liste myTests = new Liste();
-		myTests.add(new BauernhofTester());
-		myTests.add(new AnnotationTester());
-		
-		MyIterator it = myTests.iterator();
-		
-		while(it.hasNext()) {
-			((Tester) it.next()).runTests();
-			System.out.println("\n");
-		}
 	}
 }
